f7b6bc1edaebe45677956b8b568a8fe109f9dc4a
ennadyhyKaoutar <158831023+ennadyhyKaoutar@users.noreply.github.com>
2025-01-16 22:52:50 +0100

    Delete backend/backend4 directory

--- backend/backend4/backendV/src/main/java/com/GestionDossiers/demande_autorisation/repository/AutorisationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.GestionDossiers.demande_autorisation.repository;
-
-import com.GestionDossiers.demande_autorisation.entities.Autorisation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutorisationRepository extends JpaRepository<Autorisation, Long> {
-    // JpaRepository fournit déjà les méthodes CRUD par défaut, y compris findById.
-}
--- backend/backend4/backendV/src/main/java/com/GestionDossiers/demande_autorisation/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GestionDossiers.demande_autorisation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.GestionDossiers.demande_autorisation.entities.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-    Client findByEmail(String email);
-}
